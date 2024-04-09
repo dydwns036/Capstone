02c6f1d28e42728ab6f5062d658753875fc4cf66
chinhuy2310 <165356122+chinhuy2310@users.noreply.github.com>
2024-04-09 11:36:15 +0900

    Update Menu5Fragment.java

--- app/src/main/java/com/example/foodapp/Menu5Fragment.java
+++ app/src/main/java/com/example/foodapp/Menu5Fragment.java
@@ -1,9 +1,11 @@
 package com.example.foodapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,8 +14,18 @@ import androidx.fragment.app.Fragment;
 public class Menu5Fragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.menu5fragment, container, false);
+        TextView logoutTextView = view.findViewById(R.id.textViewLogout);
+        logoutTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Xử lý đăng xuất ở đây
+                // Ví dụ: Chuyển người dùng đến màn hình đăng nhập
+                Intent intent = new Intent(getActivity(), LoginActivity.class);
+                startActivity(intent);
+                getActivity().finish(); // Đóng Activity hiện tại
+            }
+        });
         return view;
     }
 }
