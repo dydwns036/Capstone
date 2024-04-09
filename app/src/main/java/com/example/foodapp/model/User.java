48d9b90c9cf6fa90bf408697a5b6c925d5b08d06
chinhuy2310 <165356122+chinhuy2310@users.noreply.github.com>
2024-04-09 11:31:20 +0900

    Add files via upload

---
+++ app/src/main/java/com/example/foodapp/model/User.java
@@ -0,0 +1,48 @@
+package com.example.foodapp.model;
+public class User {
+    private int userId;
+    private String username;
+    private String userAccname;
+    private String email;
+    private String avatarImage;
+    private String coverImage;
+    private int isAdmin;
+
+    public User(int userId, String username, String userAccname, String email, String avatarImage, String coverImage, int isAdmin) {
+        this.userId = userId;
+        this.username = username;
+        this.userAccname = userAccname;
+        this.email = email;
+        this.avatarImage = avatarImage;
+        this.coverImage = coverImage;
+        this.isAdmin = isAdmin;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getUserAccname() {
+        return userAccname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getAvatarImage() {
+        return avatarImage;
+    }
+
+    public String getCoverImage() {
+        return coverImage;
+    }
+
+    public int getIsAdmin() {
+        return isAdmin;
+    }
+}
