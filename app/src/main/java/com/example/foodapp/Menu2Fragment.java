7316aef03496a601cbdec358298c85339e448f4d
chinhuy2310 <165356122+chinhuy2310@users.noreply.github.com>
2024-04-09 11:35:07 +0900

    Update Menu2Fragment.java

--- app/src/main/java/com/example/foodapp/Menu2Fragment.java
+++ app/src/main/java/com/example/foodapp/Menu2Fragment.java
@@ -1,69 +1,79 @@
 package com.example.foodapp;
 
 import android.content.Intent;
-import android.database.Cursor;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ListView;
+import android.widget.RelativeLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.foodapp.adapter.adapterPost;
-import com.example.foodapp.data.DatabaseAll;
-import com.example.foodapp.model.Truyen;
+import com.example.foodapp.PostDetail;
+import com.example.foodapp.R;
+import com.example.foodapp.adapter.adapterPostMenu2;
+import com.example.foodapp.data.DatabaseHelper;
+import com.example.foodapp.model.Post;
 
 import java.util.ArrayList;
+import java.util.List;
 
-public class Menu2Fragment extends Fragment {
-    adapterPost adaptertruyen;
-    ListView listViewNew;
-    ArrayList<Truyen> TruyenArrayList;
-    DatabaseAll databaseDocTruyen;
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+public class Menu2Fragment extends Fragment implements adapterPostMenu2.OnPostClickListener {
+    private RecyclerView recyclerView;
+    private adapterPostMenu2 adapter;
+    private List<Post> postList;
+    private DatabaseHelper databaseHelper;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.menu2fragment, container, false);
-        databaseDocTruyen = new DatabaseAll(requireContext());
 
-        Mapping(view);
-        //listview Truyen
-        listViewNew.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+        RelativeLayout topRelativeLayout = view.findViewById(R.id.topRelativeLayout);
+        topRelativeLayout.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent intent = new Intent(requireActivity(), Content.class);
-                String tent =   TruyenArrayList.get(position).getTenTruyen();
-                String noidungt = TruyenArrayList.get(position).getNoiDung();
-                intent.putExtra("postname",tent);
-                intent.putExtra("content",noidungt);
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), AddPostActivity.class);
                 startActivity(intent);
             }
         });
+
+        recyclerView = view.findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        postList = new ArrayList<>();
+        adapter = new adapterPostMenu2(getActivity(), postList, this); // Gán listener
+        recyclerView.setAdapter(adapter);
+
+        databaseHelper = new DatabaseHelper(getActivity());
+        retrievePostsFromDatabase();
+
         return view;
     }
-    private void Mapping(View view){
+    public void onResume() {
+        super.onResume();
+        retrievePostsFromDatabase();
+    }
 
-        listViewNew = view.findViewById(R.id.listviewNew);
+    private void retrievePostsFromDatabase() {
+        postList.clear();
+        postList.addAll(databaseHelper.getAllPosts());
+        adapter.notifyDataSetChanged();
+    }
 
-        //New Post
-        TruyenArrayList = new ArrayList<>();
+    // Phương thức trong interface OnPostClickListener
+    @Override
+    public void onPostClick(int position) {
+        // Lấy đối tượng Post tại vị trí được nhấn
+        Post clickedPost = postList.get(position);
 
-        Cursor cursor1 = databaseDocTruyen.getData3();
-        while (cursor1.moveToNext()){
-            int id = cursor1.getInt(0);
-            String postname = cursor1.getString(1);
-            String content = cursor1.getString(2);
-            String anh = cursor1.getString(3);
-            int id_tk = cursor1.getInt(4);
-            TruyenArrayList.add(new Truyen(id,postname,content,anh,id_tk));
-        }
-        cursor1.close();
-
-        adaptertruyen = new adapterPost(requireContext(), TruyenArrayList);
-        listViewNew.setAdapter(adaptertruyen);
+        // Gửi Intent đến PostDetail Activity và đính kèm đối tượng Post
+        Intent intent = new Intent(getActivity(), PostDetail.class);
+        intent.putExtra("POST_DETAIL", clickedPost);
+        startActivity(intent);
     }
 
 }
