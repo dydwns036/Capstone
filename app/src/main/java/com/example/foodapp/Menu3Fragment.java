c36dccabecf739d1c3a59df8a5b4fa5dacd97b7d
chinhuy2310 <165356122+chinhuy2310@users.noreply.github.com>
2024-04-09 10:34:40 +0900

    Update Menu3Fragment.java

--- app/src/main/java/com/example/foodapp/Menu3Fragment.java
+++ app/src/main/java/com/example/foodapp/Menu3Fragment.java
@@ -13,57 +13,17 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import com.example.foodapp.adapter.adapterPost;
-import com.example.foodapp.data.DatabaseAll;
-import com.example.foodapp.model.Truyen;
+
 
 import java.util.ArrayList;
 
 public class Menu3Fragment extends Fragment {
-    adapterPost adaptertruyen;
-    ListView listViewNew;
-    ArrayList<Truyen> TruyenArrayList;
-    DatabaseAll databaseDocTruyen;
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.menu3fragment, container, false);
-        databaseDocTruyen = new DatabaseAll(requireContext());
 
-        Mapping(view);
-        //listview Truyen
-        listViewNew.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent intent = new Intent(requireActivity(), Content.class);
-                String tent =   TruyenArrayList.get(position).getTenTruyen();
-                String noidungt = TruyenArrayList.get(position).getNoiDung();
-                intent.putExtra("postname",tent);
-                intent.putExtra("content",noidungt);
-                startActivity(intent);
-            }
-        });
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.menu3fragment, container, false);
+
         return view;
     }
-    private void Mapping(View view){
 
-        listViewNew = view.findViewById(R.id.listviewNew);
-
-        //New Post
-        TruyenArrayList = new ArrayList<>();
-
-        Cursor cursor1 = databaseDocTruyen.getData3();
-        while (cursor1.moveToNext()){
-            int id = cursor1.getInt(0);
-            String postname = cursor1.getString(1);
-            String content = cursor1.getString(2);
-            String anh = cursor1.getString(3);
-            int id_tk = cursor1.getInt(4);
-            TruyenArrayList.add(new Truyen(id,postname,content,anh,id_tk));
-        }
-        cursor1.close();
-
-        adaptertruyen = new adapterPost(requireContext(), TruyenArrayList);
-        listViewNew.setAdapter(adaptertruyen);
-    }
 
 }
