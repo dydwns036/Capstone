fbdc5c0b1bfe05cf5f39197197ac6d1aa15b4d1b
chinhuy2310 <165356122+chinhuy2310@users.noreply.github.com>
2024-04-15 11:12:15 +0900

    Update Menu2Fragment.java

--- app/src/main/java/com/example/foodapp/Menu2Fragment.java
+++ app/src/main/java/com/example/foodapp/Menu2Fragment.java
@@ -2,10 +2,13 @@ package com.example.foodapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -17,7 +20,10 @@ import com.example.foodapp.PostDetail;
 import com.example.foodapp.R;
 import com.example.foodapp.adapter.adapterPostMenu2;
 import com.example.foodapp.data.DatabaseHelper;
+import com.example.foodapp.model.CircleTransform;
 import com.example.foodapp.model.Post;
+import com.example.foodapp.model.User;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -32,12 +38,29 @@ public class Menu2Fragment extends Fragment implements adapterPostMenu2.OnPostCl
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.menu2fragment, container, false);
 
+        Bundle bundle = getArguments();
+        if (bundle != null) {
+            User user = (User) bundle.getSerializable("user");
+            ImageView imgviewMAvt = view.findViewById(R.id.imageViewMyAvt);
+            if (user != null && user.getAvatarImage() != null && !user.getAvatarImage().isEmpty()) {
+                Picasso.get().load(user.getAvatarImage())
+                        .transform(new CircleTransform())
+                        .placeholder(R.drawable.custom_border2)
+                        .error(R.drawable.custom_border2)
+                        .into(imgviewMAvt);
+            } else {
+                // Nếu không có đường dẫn ảnh hoặc đường dẫn rỗng, hiển thị ảnh mặc định từ thư mục drawable
+                imgviewMAvt.setImageResource(R.drawable.user_icon2);
+            }
+        }
 
         RelativeLayout topRelativeLayout = view.findViewById(R.id.topRelativeLayout);
         topRelativeLayout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getActivity(), AddPostActivity.class);
+                User user = (User) bundle.getSerializable("user");
+                intent.putExtra("user",user);
                 startActivity(intent);
             }
         });
