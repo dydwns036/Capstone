65422c0277659258c8e4e209f6200e17389b8ff7
chinhuy2310 <165356122+chinhuy2310@users.noreply.github.com>
2024-04-16 12:08:44 +0900

    Create CircleTransform.java

---
+++ app/src/main/java/com/example/foodapp/model/CircleTransform.java
@@ -0,0 +1,38 @@
+package com.example.foodapp.model;
+import android.graphics.Bitmap;
+import com.squareup.picasso.Transformation;
+
+public class CircleTransform implements Transformation {
+    @Override
+    public Bitmap transform(Bitmap source) {
+        int size = Math.min(source.getWidth(), source.getHeight());
+
+        int x = (source.getWidth() - size) / 2;
+        int y = (source.getHeight() - size) / 2;
+
+        Bitmap squaredBitmap = Bitmap.createBitmap(source, x, y, size, size);
+        if (squaredBitmap != source) {
+            source.recycle();
+        }
+
+        Bitmap bitmap = Bitmap.createBitmap(size, size, source.getConfig());
+
+        android.graphics.Canvas canvas = new android.graphics.Canvas(bitmap);
+        android.graphics.Paint paint = new android.graphics.Paint();
+        android.graphics.BitmapShader shader = new android.graphics.BitmapShader(squaredBitmap,
+                android.graphics.BitmapShader.TileMode.CLAMP, android.graphics.BitmapShader.TileMode.CLAMP);
+        paint.setShader(shader);
+        paint.setAntiAlias(true);
+
+        float r = size / 2f;
+        canvas.drawCircle(r, r, r, paint);
+
+        squaredBitmap.recycle();
+        return bitmap;
+    }
+
+    @Override
+    public String key() {
+        return "circle";
+    }
+}
